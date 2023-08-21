314c3c09ad9c4e97afdb3a7237e4f1003502c303
Brijesh Paliwal <mpaliwal1991@gmail.com>
2023-08-21 21:16:16 +0530

    Table name in small letter

--- src/app/api/products/route.js
+++ src/app/api/products/route.js
@@ -5,7 +5,7 @@ import excuteQuery from '@/lib/db'
 export async function GET() {
  console.log('test')
  const reactdata=await excuteQuery({
-  'query':"SELECT * FROM REACT",
+  'query':"SELECT * FROM react",
   'values':[]
  })
  return NextResponse.json({ data:reactdata });
